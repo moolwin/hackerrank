a7f4146258f36a05839b05b08794eec0feb01e36
mayoo <mayoo@mayoolwins-mbp>
2023-06-25 20:44:03 +0800

    Testing to commit java file

--- Hackerrank/src/com/hackerrank/test/CollectionTest1.java
+++ Hackerrank/src/com/hackerrank/test/CollectionTest1.java
@@ -4,7 +4,8 @@ public class CollectionTest1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Testing 1");
+		System.out.println("Testing 2");
 	}
 
 }
